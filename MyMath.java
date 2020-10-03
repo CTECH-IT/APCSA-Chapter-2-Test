b1da9f9d19ec400b43200b431e2b6f8e2080d9f4
Mr. Bartucz <jobartucz@isd535.org>
2020-10-03 14:32:56 -0500

    Updated squareroot

--- MyMath.java
+++ MyMath.java
@@ -8,9 +8,15 @@ class MyMath {
 
   
 
-  public static double squareroot(double inpt) {
+  public static int squareroot(int inpt) {
 
-    return 0.0;
+    // OPTIONAL!!!!! Just for fun!!!
+    // Step 1: Just use a method from the Math class to make sure it works
+    // Step 2:
+    // use a "for" loop to start at 1 and go up and up to see if the square of your number == input
+    // if you get a square that's higher than input, just say "the input is not a perfect square!"
+
+    return 0;
 
   }
 
