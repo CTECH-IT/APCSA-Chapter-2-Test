b5c0a86aa96f35e7993ec24d3e414f5f2898a7bb
Mr. Bartucz <jobartucz@isd535.org>
2020-10-08 09:06:53 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -5,10 +5,18 @@ class Student {
   double gpa;
   String name;
 
+  // You do not need to change the default constructor, just use it as-is
   public Student() {
     age = 14;
     gpa = 4.0;
     name = "Default McDefaulterson";
+  }
+
+  // You do not need to change the overloaded constructor, just use it as-is
+  public Student(int myAge, double myGpa, String myName) {
+    age = myAge;
+    gpa = myGpa;
+    name = myName;
   }
 
 }
